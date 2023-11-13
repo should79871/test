caf773c1435837b7b41c9c6175b24e7dd96afcf3
should79871 <should79871@gmail.com>
2023-11-13 07:35:08 +0530

    third commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
   const app = express();
 const bodyParser = require('body-parser');
 const DB ="mongodb+srv://harendra5464:12jan2011@A@cluster0.fcfljwj.mongodb.net/motor?retryWrites=true&w=majority"
-mongoose.connect('mongodb+srv://harendra5464:3k42nLJVQ1R5X1LV@cluster0.fcfljwj.mongodb.net/motor?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://harendra5464:ARBzxfNDlam114MK@cluster0.fcfljwj.mongodb.net/motor?retryWrites=true&w=majority',{
    useNewUrlParser: true,
           useUnifiedTopology: true,
 })
@@ -23,9 +23,40 @@ mongoose.connect('mongodb+srv://harendra5464:3k42nLJVQ1R5X1LV@cluster0.fcfljwj.m
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', async (req, res) => {
+//   try {
+//     // Fetch data from MongoDB
+//     const data = await YourModel.find();
+
+//     // Render the HTML page and pass the data to it
+//     res.sendFile(__dirname + '/index.hbs',{data});
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
+
+
+
+app.set('view engine','ejs')
+app.get("/",async(req,res)=>{
+   const data = await YourModel.find();
+   
+   res.render('index',{data},console.log(data.isMotorOn))
+  
+})
+// app.get('/', (req, res) =>
+// {
+//   try {
+//     // Fetch data from MongoDB
+//     //const data = await YourModel.find();
+//     const data = await YourModel.find();
+//     res.sendFile(__dirname + '/index.html',{});
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
 
 //app.use(express.static('public'));
 app.use(express.json());
@@ -71,43 +102,13 @@ const playlistSchema= new mongoose.Schema({
    
 
     const YourModel = mongoose.model('motor', new mongoose.Schema({
-  // Define your schema here
+
+  isMotorOn: Boolean,
   // Example: { name: String, age: Number }
 
 }), 'playlists');
 
-    app.get('/', async (req, res) => {
-  try {
-    // Fetch data from MongoDB
-    const data = await YourModel.find();
 
-    // Render the HTML page and pass the data to it
-    res.send(`
-      <html>
-        <head>
-          <title>MongoDB Data</title>
-        </head>
-        <body>
-          <h1>Data from MongoDB</h1>
-          <ul id="dataList"></ul>
-          <script>
-            // Use JavaScript to dynamically populate the data in the HTML page
-            const dataList = document.getElementById('dataList');
-            const data = ${JSON.stringify(data)};
-            data.forEach(item => {
-              const li = document.createElement('li');
-              li.textContent = item.isMotorOn; // Replace 'name' with the field you want to display
-              dataList.appendChild(li);
-            });
-          </script>
-        </body>
-      </html>
-    `);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
-});
 
 //
 
@@ -118,6 +119,7 @@ const playlistSchema= new mongoose.Schema({
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  
 });
     
 
@@ -130,6 +132,7 @@ app.post('/runFunction', (req, res) => {
 });
 
 app.post('/runFunction2', (req, res) => {
+  
   // Your Node.js function logic goes here
   const result = runYourFunction2(); // Replace with your actual function
 
@@ -138,8 +141,10 @@ app.post('/runFunction2', (req, res) => {
 });
 
 function runYourFunction() {
+  
    // Replace this with your actual function logic
    const updateDocument= async(_id)=>{
+    
       try{
          const result = await Playlist.updateOne({_id},{
            
@@ -179,6 +184,7 @@ function runYourFunction2() {
    }
 
    updateDocument("654de9edd47767a2a0b3c4cd")
+   
 }
 
 
